dacc700f71944348a5b7502552a11f76bc6f7994
Nguyễn Hữu Nghĩa <122595940+huunghia369@users.noreply.github.com>
2025-03-11 14:37:08 +0700

    Frontend Setup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 
 import authRoutes from "./routes/auth.route.js";
-import userRoutes from "./routes/user.route.js";
-import postRoutes from "./routes/post.route.js";
-import notificationRoutes from "./routes/notification.route.js";
 import connectionRoutes from "./routes/connection.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import postRoutes from "./routes/post.route.js";
+import userRoutes from "./routes/user.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -14,6 +15,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
 app.use(cookieParser()); // parse cookies
