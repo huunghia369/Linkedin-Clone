ded6a636ced7ae88972aa7f64ab877e8990816c4
Nguyễn Hữu Nghĩa <122595940+huunghia369@users.noreply.github.com>
2025-03-13 16:55:15 +0700

    Project completed

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import dotenv from "dotenv";
-import express from "express";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
-import connectionRoutes from "./routes/connection.route.js";
-import notificationRoutes from "./routes/notification.route.js";
-import postRoutes from "./routes/post.route.js";
 import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import connectionRoutes from "./routes/connection.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -15,13 +16,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    }),
+  );
+}
 
 app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
 app.use(cookieParser()); // parse cookies
@@ -32,7 +36,15 @@ app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/notifications", notificationRoutes);
 app.use("/api/v1/connections", connectionRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   connectDB();
-}); 
+});
